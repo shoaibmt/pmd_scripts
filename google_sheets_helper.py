2657386823d953d9d0a671f4fed575c0a592f3bb
shoaibmt <shoaibmt3@gmail.com>
2025-04-06 21:07:27 +0500

    pmd - update secrects

--- google_sheets_helper.py
+++ google_sheets_helper.py
@@ -1,30 +1,16 @@
 import os
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
 def google_sheet_connection():
     SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-
-    creds = None
-    # Load credentials from token.json if it exists
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    
-    # If there are no valid credentials available, prompt user to log in
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            # This prompts the user to log in with their Google account
-            flow = InstalledAppFlow.from_client_secrets_file('client_secret.json', SCOPES)
-            creds = flow.run_local_server(port=8080)
-        
-        # Save the credentials to token.json for future use
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-    
-    # Connect to the Google Sheets API
+    creds = Credentials(
+        token=None,
+        refresh_token=os.getenv("GOOGLE_REFRESH_TOKEN"),
+        token_uri="https://oauth2.googleapis.com/token",
+        client_id=os.getenv("GOOGLE_CLIENT_ID"),
+        client_secret=os.getenv("GOOGLE_CLIENT_SECRET")
+    )
     service = build('sheets', 'v4', credentials=creds)
     return service
